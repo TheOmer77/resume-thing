f4944cdb822cd82b2858031f82c88e7076487a58
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-31 17:30:05 +0300

    Render react component in PDF route

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,16 +1,22 @@
 import puppeteer, { type PDFMargin } from 'puppeteer-core';
 import chromium from '@sparticuz/chromium';
 
-const EXAMPLE_URL =
-    'https://palette-generator-thing.vercel.app/?primary=1740ea&neutral=neutral30',
-  EXAMPLE_MARGINS = {
-    bottom: '0.4in',
-    left: '0.4in',
-    right: '0.4in',
-    top: '0.4in',
-  } satisfies PDFMargin;
+import { ExampleContent } from '@/components/example';
+
+const EXAMPLE_MARGINS = {
+  bottom: '0.4in',
+  left: '0.4in',
+  right: '0.4in',
+  top: '0.4in',
+} satisfies PDFMargin;
 
 export const GET = async () => {
+  // Next.js complains when you import 'react-dom/server' directly
+  const { renderToStaticMarkup } = await import('react-dom/server');
+
+  const html = renderToStaticMarkup(ExampleContent());
+  const dataUrl = `data:text/html,${encodeURIComponent(html)}`;
+
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -19,7 +25,7 @@ export const GET = async () => {
   });
   const page = await browser.newPage();
   console.log('DEBUG | browser.newPage passed');
-  await page.goto(EXAMPLE_URL, { waitUntil: 'domcontentloaded' });
+  await page.goto(dataUrl, { waitUntil: 'domcontentloaded' });
   console.log('DEBUG | page.goTo passed');
 
   const pdf = await page.pdf({
