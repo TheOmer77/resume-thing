5490eb465b7118b03c04cfe8188748494603ed59
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-31 12:38:27 +0300

    Add some PostCSS plugins

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-mixins': {},
     tailwindcss: {},
+    'postcss-hover-media-feature': {},
+    'postcss-viewport-unit-fallback': {},
+    autoprefixer: {},
   },
 };
 
