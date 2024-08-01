12640de2352408e0e2104afcce88fce00cfbed4d
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-01 16:17:14 +0300

    Preserve `pre` background color in print

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,16 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: {
+              'print-color-adjust': 'exact !important',
+              '-webkit-print-color-adjust': 'exact !important',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [tailwindTypography, tailwindAnimate],
