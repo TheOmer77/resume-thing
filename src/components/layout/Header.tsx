2ccf90e5bbe44a9ba49dd767389603fb3349526c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-01 17:38:00 +0300

    Hide header on print

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { PDFDownloadButton } from './PDFDownloadButton';
 
 export const Header = () => (
-  <header className='fixed top-0 z-50 w-full bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+  <header className='fixed top-0 z-50 w-full bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60 print:hidden'>
     <div className='container flex h-16 max-w-screen-2xl items-center'>
       <div className='mr-4 flex items-center text-2xl font-extrabold tracking-tight text-primary lg:me-6'>
         PDF Thing
