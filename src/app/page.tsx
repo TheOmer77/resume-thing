aeb9e13886b8e5bddf4cff4c190f15152f98692f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-02 12:50:13 +0300

    Add PDFViewer to page with the generated PDF

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Header } from '@/components/layout/Header';
+import { PDFViewer } from '@/components/PDFViewer';
 
 const Page = () => (
   <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
     <Header />
-    Hello world.
+    <PDFViewer file='/api/pdf' />
   </div>
 );
 
