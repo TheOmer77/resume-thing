2be8954b99142eb0ed7b828b7132c1bcdcf38fc9
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-02 12:11:12 +0300

    Use local Inter font instead of CDN in pdf route
    Don't rely on a CDN for importing the Inter font, instead rely on local
    files via the `inter-ui` package.

--- src/app/api/pdf/route.tsx
+++ src/app/api/pdf/route.tsx
@@ -5,34 +5,31 @@ import tailwindTypography from '@tailwindcss/typography';
 import { ExampleContent } from '@/components/example';
 import { generateTailwindCss } from '@/lib/generateTailwindCss';
 import { EXAMPLE_MARGINS } from '@/constants/margins';
+import { interCss } from '@/constants/inter';
 
 export const GET = async () => {
   // Next.js complains when you import 'react-dom/server' directly
   const { renderToStaticMarkup } = await import('react-dom/server');
 
   const html = renderToStaticMarkup(<ExampleContent />);
-  const css = await generateTailwindCss(
-    html,
-    "@import url('https://rsms.me/inter/inter.css');",
-    {
-      plugins: [tailwindTypography],
-      theme: {
-        extend: {
-          fontFamily: { sans: 'Inter, sans-serif' },
-          typography: {
-            DEFAULT: {
-              css: {
-                pre: {
-                  'print-color-adjust': 'exact !important',
-                  '-webkit-print-color-adjust': 'exact !important',
-                },
+  const css = await generateTailwindCss(html, interCss, {
+    plugins: [tailwindTypography],
+    theme: {
+      extend: {
+        fontFamily: { sans: 'Inter, sans-serif' },
+        typography: {
+          DEFAULT: {
+            css: {
+              pre: {
+                'print-color-adjust': 'exact !important',
+                '-webkit-print-color-adjust': 'exact !important',
               },
             },
           },
         },
       },
-    }
-  );
+    },
+  });
 
   const browser = await puppeteer.launch({
     args: chromium.args,
