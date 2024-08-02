dbadc8159f87625762299ce363c4c9030bcfd735
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-02 13:00:27 +0300

    Add some required config for `react-pdf`

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
   },
   webpack: config => {
+    config.resolve.alias.canvas = false;
     config.module.rules.push({ test: /\.woff2$/i, type: 'asset/resource' });
     return config;
   },
