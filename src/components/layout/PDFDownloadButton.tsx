63a66e3f8e98b39aa5db2cbc3905e5d78b951cf4
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-02 13:38:56 +0300

    Use usePDFFile in PDFDownloadButton
    This allows for an instant download on click, since there's no need to
    fetch the PDF file again.

--- src/components/layout/PDFDownloadButton.tsx
+++ src/components/layout/PDFDownloadButton.tsx
@@ -1,12 +1,33 @@
+'use client';
+
+import { useCallback } from 'react';
 import { DownloadIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { usePDFFile } from '@/hooks/usePDFFile';
 
-export const PDFDownloadButton = () => (
-  <Button className='size-10 p-0 sm:w-auto sm:px-4 sm:py-2' asChild>
-    <a href='/api/pdf' download='thing.pdf'>
+export const PDFDownloadButton = () => {
+  const file = usePDFFile();
+
+  const handleClick = useCallback(() => {
+    if (!file) return;
+
+    const a = document.createElement('a');
+    const url = window.URL.createObjectURL(file);
+    a.href = url;
+    a.download = 'thing.pdf';
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }, [file]);
+
+  if (!file) return null;
+  return (
+    <Button
+      className='size-10 p-0 sm:w-auto sm:px-4 sm:py-2'
+      onClick={handleClick}
+    >
       <DownloadIcon className='size-4 sm:me-2' />
       <span className='hidden sm:inline'>Download</span>
-    </a>
-  </Button>
-);
+    </Button>
+  );
+};
