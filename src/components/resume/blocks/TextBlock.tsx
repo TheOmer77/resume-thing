25a8be74dd72ce8f6141a82a7533a45f3eaad18e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-04 12:46:32 +0300

    Move text block data to resumeBlocks constants

--- src/components/resume/blocks/TextBlock.tsx
+++ src/components/resume/blocks/TextBlock.tsx
@@ -1,80 +1,14 @@
 import { cn } from '@/lib/utils';
-import type { TextBlockData } from '@/types/blocks';
+import { resumeBlocks } from '@/constants/resume/blocks';
 
 import { MarkdownText } from '../MarkdownText';
 
-// TODO: Don't hardcode content
-const TEMP_TEXT_BLOCKS = [
-  {
-    id: 'md-test',
-    type: 'text',
-    content: {
-      text: `Regular. **Bold.** *Italic.* ***Bold AND italic!*** ~~Strike this.~~ **~~Strike and also bold.~~** [Link to Google](https://www.google.com/)
-
-- One
-- Two
-- Three
-
-1. One
-2. Two
-3. Three`,
-    },
-  },
-  {
-    id: 'dummy-summary',
-    type: 'text',
-    content: {
-      text: 'In dui lectus, molestie lacinia lectus et, elementum fringilla lorem. Morbi elementum massa a erat finibus commodo. Duis id porttitor tortor. Praesent mauris ipsum, mattis nec pretium nec, semper convallis nisl. Aliquam vulputate iaculis dui eu blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra.',
-      lead: true,
-    },
-  },
-  {
-    id: 'dummy-skills',
-    type: 'text',
-    content: {
-      text: `
-User Research \\
-Design Sprints \\
-Concept Design \\
-Prototyping \\
-Usability Testing \\
-Design Systems \\
-Agile Practices`,
-    },
-  },
-  {
-    id: 'dummy-tools',
-    type: 'text',
-    content: {
-      text: `
-Miro - FigJam \\
-Figma - Penpot \\
-Abstract \\
-Maze / Usertesting \\
-Webflow - Framer \\
-Notion / GDocs \\
-Zeroheight \\
-Jira / Taiga`,
-    },
-  },
-  {
-    id: 'dummy-languages',
-    type: 'text',
-    content: {
-      text: `
-Spanish (Native) \\
-English (Fluent)`,
-    },
-  },
-] satisfies TextBlockData[];
-
 type TEMP_TextBlockProps = {
   TEMP_blockId: string;
-  type: 'text';
 };
 
 export const TextBlock = ({ TEMP_blockId }: TEMP_TextBlockProps) => {
-  const block = TEMP_TEXT_BLOCKS.find(({ id }) => id === TEMP_blockId);
+  const block = resumeBlocks.find(({ id }) => id === TEMP_blockId);
   if (!block) return null;
 
   return (
--- src/constants/resume/blocks.ts
+++ src/constants/resume/blocks.ts
@@ -1,12 +1,75 @@
 import type { BlockData } from '@/types/blocks';
 
 export const resumeBlocks = [
+  /** Test block, to be removed */
+  {
+    id: 'md-test',
+    type: 'text',
+    content: {
+      text: `Regular. **Bold.** *Italic.* ***Bold AND italic!*** ~~Strike this.~~ **~~Strike and also bold.~~** [Link to Google](https://www.google.com/)
+
+- One
+- Two
+- Three
+
+1. One
+2. Two
+3. Three`,
+    },
+  },
+
   {
     id: 'dummy-title',
     type: 'title',
     content: {
       title: 'Name Lastname',
       subtitle: 'Job Title',
+    },
+  },
+  {
+    id: 'dummy-summary',
+    type: 'text',
+    content: {
+      text: 'In dui lectus, molestie lacinia lectus et, elementum fringilla lorem. Morbi elementum massa a erat finibus commodo. Duis id porttitor tortor. Praesent mauris ipsum, mattis nec pretium nec, semper convallis nisl. Aliquam vulputate iaculis dui eu blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra.',
+      lead: true,
+    },
+  },
+  {
+    id: 'dummy-skills',
+    type: 'text',
+    content: {
+      text: `
+User Research \\
+Design Sprints \\
+Concept Design \\
+Prototyping \\
+Usability Testing \\
+Design Systems \\
+Agile Practices`,
+    },
+  },
+  {
+    id: 'dummy-tools',
+    type: 'text',
+    content: {
+      text: `
+Miro - FigJam \\
+Figma - Penpot \\
+Abstract \\
+Maze / Usertesting \\
+Webflow - Framer \\
+Notion / GDocs \\
+Zeroheight \\
+Jira / Taiga`,
+    },
+  },
+  {
+    id: 'dummy-languages',
+    type: 'text',
+    content: {
+      text: `
+Spanish (Native) \\
+English (Fluent)`,
     },
   },
 ] satisfies BlockData[];
