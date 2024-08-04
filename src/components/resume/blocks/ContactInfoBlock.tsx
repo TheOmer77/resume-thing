8204d61e283ae9d5a02c2f1dd0442a32ad37c0ea
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-04 19:02:05 +0300

    Update ContactInfoBlock to use a grid layout

--- src/components/resume/blocks/ContactInfoBlock.tsx
+++ src/components/resume/blocks/ContactInfoBlock.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { MailIcon, MapPinIcon, PhoneIcon } from 'lucide-react';
 
 import { resumeBlocks } from '@/constants/resume/blocks';
@@ -18,17 +19,14 @@ export const ContactInfoBlock = ({ blockId }: BlockProps) => {
   if (!block) return null;
 
   return (
-    <div className='pb-6'>
+    <div className='grid grid-cols-[auto,1fr] items-center gap-x-2'>
       {block.content.items.map(({ text, icon }, idx) => {
         const Icon = icons[icon as keyof typeof icons];
         return (
-          <div
-            key={`contactInfo-${idx}`}
-            className='flex flex-row items-center gap-2'
-          >
+          <Fragment key={`contactInfo-${idx}`}>
             {Icon && <Icon className='size-[1.125rem]' />}
             <span>{text}</span>
-          </div>
+          </Fragment>
         );
       })}
     </div>
