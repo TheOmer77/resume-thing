7cd8e52936beb35977e422c59943d292c4e1a183
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-04 20:04:50 +0300

    Revert "Update ContactInfoBlock to use a grid layout"
    This reverts commit 8204d61e283ae9d5a02c2f1dd0442a32ad37c0ea.

--- src/components/resume/blocks/ContactInfoBlock.tsx
+++ src/components/resume/blocks/ContactInfoBlock.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { MailIcon, MapPinIcon, PhoneIcon } from 'lucide-react';
 
 import { resumeBlocks } from '@/constants/resume/blocks';
@@ -19,14 +18,17 @@ export const ContactInfoBlock = ({ blockId }: BlockProps) => {
   if (!block) return null;
 
   return (
-    <div className='grid grid-cols-[auto,1fr] items-center gap-x-2'>
+    <div className='pb-6'>
       {block.content.items.map(({ text, icon }, idx) => {
         const Icon = icons[icon as keyof typeof icons];
         return (
-          <Fragment key={`contactInfo-${idx}`}>
+          <div
+            key={`contactInfo-${idx}`}
+            className='flex flex-row items-center gap-2'
+          >
             {Icon && <Icon className='size-[1.125rem]' />}
             <span>{text}</span>
-          </Fragment>
+          </div>
         );
       })}
     </div>
