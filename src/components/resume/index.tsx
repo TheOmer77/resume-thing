8b198e371b6a8681376f6e855338a3bbd6a6d7dd
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-05 14:23:46 +0300

    Change `--inverted-radius` var to be in inches

--- src/components/resume/index.tsx
+++ src/components/resume/index.tsx
@@ -10,7 +10,7 @@ export const ResumeRoot = () => (
       'relative grid min-h-screen break-after-avoid grid-cols-[var(--secondary-col-width),1fr]',
       'grid-rows-[auto,1fr] before:fixed before:w-[--secondary-col-width]',
       'before:start-0 before:top-0 before:-z-10 before:h-full before:bg-primary',
-      '[--inverted-radius:4rem] after:absolute after:start-[--secondary-col-width]',
+      '[--inverted-radius:0.6in] after:absolute after:start-[--secondary-col-width]',
       'after:top-0 after:h-full after:w-[--inverted-radius] after:rounded-bl-[calc(var(--inverted-radius)/2)]',
       'after:rounded-tl-[calc(var(--inverted-radius)/2)] after:shadow-[calc(var(--inverted-radius)/-2)_0_0_0_theme(colors.primary)]',
       'after:-z-20 print:[-webkit-print-color-adjust:exact] print:[print-color-adjust:exact]'
