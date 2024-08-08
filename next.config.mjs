399124ef65b4b1577508d3f8f6e0d6fb0ec7e375
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-08 21:35:55 +0300

    Remove `@sparticuz/chromium` from external packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
-  },
+  experimental: { serverComponentsExternalPackages: ['puppeteer-core'] },
   webpack: config => {
     config.resolve.alias.canvas = false;
     return config;
