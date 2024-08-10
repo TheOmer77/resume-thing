af21e5d2e732f7a432e7ba18967142860f54629e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-10 16:56:44 +0300

    Remove `id` column from title block content schema

--- src/db/schema/blockContent/title.ts
+++ src/db/schema/blockContent/title.ts
@@ -1,13 +1,10 @@
 import { pgTable, text } from 'drizzle-orm/pg-core';
-import { createId } from '@paralleldrive/cuid2';
 
 import { block } from '../block';
 
 export const blockContentTitle = pgTable('block_content_title', {
-  id: text('id').primaryKey().$default(createId),
   blockId: text('block_id')
-    .unique()
-    .notNull()
+    .primaryKey()
     .references(() => block.id, { onDelete: 'cascade' }),
 
   title: text('title').notNull(),
