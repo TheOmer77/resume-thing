80e33eb2366d6f433806476535d27aecfaf45a6f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-10 16:56:28 +0300

    Remove `id` column from text block content schema

--- src/db/schema/blockContent/text.ts
+++ src/db/schema/blockContent/text.ts
@@ -1,13 +1,10 @@
 import { boolean, pgTable, text } from 'drizzle-orm/pg-core';
-import { createId } from '@paralleldrive/cuid2';
 
 import { block } from '../block';
 
 export const blockContentText = pgTable('block_content_text', {
-  id: text('id').primaryKey().$default(createId),
   blockId: text('block_id')
-    .unique()
-    .notNull()
+    .primaryKey()
     .references(() => block.id, { onDelete: 'cascade' }),
 
   text: text('text').notNull(),
