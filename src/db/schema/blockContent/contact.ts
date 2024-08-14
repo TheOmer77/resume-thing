4e054403f62b43a7d508f5047cad3750f40c7cbb
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-14 14:33:37 +0300

    Add schemas for contact info block and its items

---
+++ src/db/schema/blockContent/contact.ts
@@ -0,0 +1,16 @@
+import { pgEnum, pgTable, text } from 'drizzle-orm/pg-core';
+
+import { block } from '../block';
+
+export const blockContentContactOrientation = pgEnum(
+  'block_content_contact_orientation',
+  ['horizontal', 'vertical']
+);
+
+export const blockContentContact = pgTable('block_content_contact', {
+  blockId: text('block_id')
+    .primaryKey()
+    .references(() => block.id, { onDelete: 'cascade' }),
+
+  orientation: blockContentContactOrientation('orientation'),
+});
--- src/db/schema/blockContent/index.ts
+++ src/db/schema/blockContent/index.ts
@@ -1,3 +1,5 @@
+export * from './contact';
+export * from './contactItem';
 export * from './experience';
 export * from './text';
 export * from './title';
