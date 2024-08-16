369b2082c2c9739d75c9a5ddaa37da6eae6fea9c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-16 17:19:49 +0300

    Add schemas for section block content & children

--- src/db/schema/blockContent/index.ts
+++ src/db/schema/blockContent/index.ts
@@ -1,5 +1,7 @@
 export * from './contact';
 export * from './contactItem';
 export * from './experience';
+export * from './section';
+export * from './sectionChild';
 export * from './text';
 export * from './title';
