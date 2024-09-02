edac645c05da0b101966e87415da14667ff2fd01
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 20:19:57 +0300

    Replace home page with list of resumes

--- src/app/(viewer)/page.tsx
+++ src/app/(viewer)/page.tsx
@@ -1,14 +1,43 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
 import { Header } from '@/components/layout/Header';
-import { PDFDownloadButton } from '@/components/pdf/PDFDownloadButton';
-import { PDFViewer } from '@/components/pdf/PDFViewer';
+import { getResumes } from '@/db/queries/resume';
 
-const Page = () => (
-  <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
-    <Header>
-      <PDFDownloadButton />
-    </Header>
-    <PDFViewer />
-  </div>
-);
+const HomePage = async () => {
+  const resumes = await getResumes();
+  return (
+    <div className='min-h-dvh bg-muted/40 pt-16'>
+      <Header />
+      {/* THIS UI IS TEMPORARY!! */}
+      <main className='mx-auto w-full max-w-screen-2xl p-4'>
+        <h1 className='mb-4 text-3xl font-bold tracking-tight'>Your resumes</h1>
+        <ul>
+          {resumes.map(({ id, title, updatedAt }) => (
+            <li key={id} className='w-full'>
+              <Button
+                asChild
+                variant='ghost'
+                className='h-auto w-full flex-col items-start'
+              >
+                <Link href={`/view/${id}`}>
+                  <span className='text-lg font-semibold'>{title}</span>
+                  <span className='text-sm text-muted-foreground'>
+                    Last updated{' '}
+                    {updatedAt.toLocaleDateString('en-US', {
+                      day: 'numeric',
+                      month: 'short',
+                      year: 'numeric',
+                    })}
+                  </span>
+                </Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
 
-export default Page;
+export default HomePage;
