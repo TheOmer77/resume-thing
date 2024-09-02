5132be5918dbd4086d2794d11d7d8d35e1b90c48
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 20:23:41 +0300

    Add PDF viewer to resume view page

--- src/app/(viewer)/view/[id]/page.tsx
+++ src/app/(viewer)/view/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { Header } from '@/components/layout/Header';
+import { PDFDownloadButton } from '@/components/pdf/PDFDownloadButton';
+import { PDFViewer } from '@/components/pdf/PDFViewer';
 
-const Page = () => (
+type ResumeViewPageProps = { params: { id: string } };
+
+const ResumeViewPage = ({ params: { id } }: ResumeViewPageProps) => (
   <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
-    <Header title='View resume' />
-    Content of this page TBD
+    <Header title='View resume'>
+      <PDFDownloadButton resumeId={id} />
+    </Header>
+    <PDFViewer resumeId={id} />
   </div>
 );
 
-export default Page;
+export default ResumeViewPage;
