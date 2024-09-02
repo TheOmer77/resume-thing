68bfc44989ae8935df84282cd0d93c1eb16a67d7
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 14:59:24 +0300

    Update Header component
    Add children and optional title props.

--- src/app/(viewer)/page.tsx
+++ src/app/(viewer)/page.tsx
@@ -1,9 +1,12 @@
 import { Header } from '@/components/layout/Header';
+import { PDFDownloadButton } from '@/components/pdf/PDFDownloadButton';
 import { PDFViewer } from '@/components/pdf/PDFViewer';
 
 const Page = () => (
   <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
-    <Header />
+    <Header>
+      <PDFDownloadButton />
+    </Header>
     <PDFViewer />
   </div>
 );
