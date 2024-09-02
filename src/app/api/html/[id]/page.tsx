5fdd3409bb8f345a8a3ef472630e9f8130b316fb
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 20:23:02 +0300

    Update PDF & HTML routes to accept resume ID

---
+++ src/app/api/html/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { getBlocks } from '@/db/queries/block';
+
+import { Block } from '../_components/blocks';
+import {
+  PrimaryColumn,
+  ResumeRoot,
+  SecondaryColumn,
+  SecondaryHeaderRow,
+} from '../_components/layout';
+import { BlocksProvider } from '../context';
+
+type ResumeHtmlPageProps = { params: { id: string } };
+
+const ResumeHtmlPage = async ({ params: { id } }: ResumeHtmlPageProps) => {
+  const blocks = await getBlocks({ resumeId: id });
+  return (
+    <BlocksProvider value={blocks}>
+      <ResumeRoot>
+        <SecondaryHeaderRow>
+          {/* TODO: Secondary header row blocks */}
+        </SecondaryHeaderRow>
+        <PrimaryColumn>
+          {blocks.map(({ id }) => (
+            <Block key={id} blockId={id} />
+          ))}
+        </PrimaryColumn>
+        <SecondaryColumn>{/* TODO: Secondary column blocks */}</SecondaryColumn>
+      </ResumeRoot>
+    </BlocksProvider>
+  );
+};
+
+export default ResumeHtmlPage;
