73421bfdb58cba4fb10b69496da753f27d262455
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 15:02:17 +0300

    Add resume ID and order columns to block schema

--- src/db/schema/block.ts
+++ src/db/schema/block.ts
@@ -1,9 +1,15 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { pgTable, smallint, text, unique } from 'drizzle-orm/pg-core';
 import type { InferSelectModel } from 'drizzle-orm';
 import { createId } from '@paralleldrive/cuid2';
 
-export const block = pgTable('block', {
-  id: text('id').primaryKey().$default(createId),
-});
+export const block = pgTable(
+  'block',
+  {
+    id: text('id').primaryKey().$default(createId),
+    resumeId: text('resume_id').notNull(),
+    order: smallint('order'),
+  },
+  table => ({ uniqueOrderPerResume: unique().on(table.resumeId, table.order) })
+);
 
 export type Block = InferSelectModel<typeof block>;
