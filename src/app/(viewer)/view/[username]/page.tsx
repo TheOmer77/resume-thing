167c43a325233faf965a1233a9833b5b6a7d7e07
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 15:04:27 +0300

    Add page for viewing a resume by ID

---
+++ src/app/(viewer)/view/[username]/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/layout/Header';
+
+const Page = () => (
+  <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <Header title='View resume' />
+    Content of this page TBD
+  </div>
+);
+
+export default Page;
