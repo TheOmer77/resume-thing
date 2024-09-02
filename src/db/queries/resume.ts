3ece5ccebc8c8887f5043fc0f5ddb745fd00ed1d
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-02 18:48:09 +0300

    Order resumes by `updatedAt` in `getResumes`

--- src/db/queries/resume.ts
+++ src/db/queries/resume.ts
@@ -1,9 +1,10 @@
-import { eq } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 
 import { db } from '@/db';
 import { resume } from '@/db/schema';
 
-export const getResumes = async () => await db.select().from(resume);
+export const getResumes = async () =>
+  await db.select().from(resume).orderBy(desc(resume.updatedAt));
 
 export const getResumeById = async (id: string) =>
   (await db.select().from(resume).where(eq(resume.id, id)))[0];
