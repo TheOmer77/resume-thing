ae05ef568483560bb3c58338f6206a0d9effaf3d
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-03 17:32:25 +0300

    Include `inHeaderRow` and `inSecondaryCol` in `getBlocks`

--- src/db/queries/block.ts
+++ src/db/queries/block.ts
@@ -1,4 +1,14 @@
-import { and, asc, eq, isNotNull, or, SQL, sql } from 'drizzle-orm';
+import {
+  and,
+  asc,
+  desc,
+  eq,
+  isNotNull,
+  isNull,
+  or,
+  SQL,
+  sql,
+} from 'drizzle-orm';
 import type { PgColumn } from 'drizzle-orm/pg-core';
 
 import { db } from '@/db';
@@ -7,12 +17,11 @@ import { jsonBuildObject } from '@/lib/drizzle';
 import { queryMap } from '@/constants/queries/block';
 import type { BlockData } from '@/types/blocks';
 
-export const getBlocks = async ({ resumeId }: { resumeId?: string } = {}) => {
-  const blockContentWhere = or(
-    ...queryMap.map(({ schema }) => isNotNull(schema.blockId))
-  );
-  /** Required for child schemas to be fetched properly. */
-  const groupBy = queryMap
+const blockContentWhere = or(
+  ...queryMap.map(({ schema }) => isNotNull(schema.blockId))
+);
+/** Required for child schemas to be fetched properly. */
+const groupBy = queryMap
     .reduce<PgColumn[]>((arr, { schema, properties }) => {
       return [
         ...arr,
@@ -20,13 +29,22 @@ export const getBlocks = async ({ resumeId }: { resumeId?: string } = {}) => {
         ...Object.values(properties).filter(value => !(value instanceof SQL)),
       ];
     }, [])
-    .flat();
+    .flat(),
+  orderBy = [
+    isNull(block.order), // Blocks without order are last
+    desc(block.inHeaderRow),
+    asc(block.inSecondaryCol),
+    asc(block.order),
+  ];
 
+export const getBlocks = async ({ resumeId }: { resumeId?: string } = {}) => {
   const initialQuery = db
     .select({
       id: block.id,
       resumeId: block.resumeId,
       order: block.order,
+      inHeaderRow: block.inHeaderRow,
+      inSecondaryCol: block.inSecondaryCol,
       type: sql`CASE ${sql.join(
         queryMap.map(
           ({ schema, type }) =>
@@ -62,7 +80,7 @@ export const getBlocks = async ({ resumeId }: { resumeId?: string } = {}) => {
         : blockContentWhere
     )
     .groupBy(block.id, ...groupBy)
-    .orderBy(asc(block.order))) as BlockData[];
+    .orderBy(...orderBy)) as BlockData[];
 
   return result;
 };
