d483148ccc99eb3a6e406b27f694514068eff2e6
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-03 16:01:36 +0300

    Add route for fetching blocks by resume ID

--- src/app/api/[[...route]]/blocks.ts
+++ src/app/api/[[...route]]/blocks.ts
@@ -2,7 +2,12 @@ import { Hono } from 'hono';
 
 import { getBlocks } from '@/db/queries/block';
 
-export const blocksRouter = new Hono().get('/', async ctx => {
-  const result = await getBlocks();
-  return ctx.json(result);
-});
+export const blocksRouter = new Hono()
+  .get('/', async ctx => {
+    const result = await getBlocks();
+    return ctx.json(result);
+  })
+  .get('/:id', async ctx => {
+    const result = await getBlocks({ resumeId: ctx.req.param('id') });
+    return ctx.json(result);
+  });
