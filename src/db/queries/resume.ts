3750015b8aee3f7cb76c9fb3fe6fcb37098494cb
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-05 19:11:01 +0300

    Correctly type `getResumeById`
    Add `undefined` to return type of `getResumeById`, since calling this
    function with a non existent resume ID will return undefined.

--- src/db/queries/resume.ts
+++ src/db/queries/resume.ts
@@ -1,10 +1,10 @@
 import { desc, eq } from 'drizzle-orm';
 
 import { db } from '@/db';
-import { resume } from '@/db/schema';
+import { resume, type Resume } from '@/db/schema';
 
 export const getResumes = async () =>
   await db.select().from(resume).orderBy(desc(resume.updatedAt));
 
-export const getResumeById = async (id: string) =>
+export const getResumeById = async (id: string): Promise<Resume | undefined> =>
   (await db.select().from(resume).where(eq(resume.id, id)))[0];
