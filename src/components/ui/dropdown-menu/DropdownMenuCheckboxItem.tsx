f0efbc0337842acb652092c24bf9e3b3c9431d08
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-05 19:35:44 +0300

    Add Shadcn Dropdown Menu

---
+++ src/components/ui/dropdown-menu/DropdownMenuCheckboxItem.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from 'react';
+import { CheckboxItem, ItemIndicator } from '@radix-ui/react-dropdown-menu';
+import { Check } from 'lucide-react';
+
+import { cn } from '@/lib/cn';
+
+export const DropdownMenuCheckboxItem = forwardRef<
+  ElementRef<typeof CheckboxItem>,
+  ComponentPropsWithoutRef<typeof CheckboxItem>
+>(({ className, children, checked, ...props }, ref) => (
+  <CheckboxItem
+    ref={ref}
+    className={cn(
+      'relative flex cursor-default select-none items-center rounded-sm py-1.5 pe-2 ps-8 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      className
+    )}
+    checked={checked}
+    {...props}
+  >
+    <span className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
+      <ItemIndicator>
+        <Check className='h-4 w-4' />
+      </ItemIndicator>
+    </span>
+    {children}
+  </CheckboxItem>
+));
+DropdownMenuCheckboxItem.displayName = CheckboxItem.displayName;
