b6bc2d5c6cf0b24d951274683178338c4447ca05
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-09 17:12:08 +0300

    Add "not found" error handling in resume API route

--- src/app/api/[[...route]]/resumes.ts
+++ src/app/api/[[...route]]/resumes.ts
@@ -1,13 +1,19 @@
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 
 import { getResumeById, getResumes } from '@/db/queries/resume';
 
 export const resumesRouter = new Hono()
   .get('/', async ctx => {
-    const result = await getResumes();
-    return ctx.json(result);
+    const data = await getResumes();
+    return ctx.json(data);
   })
   .get('/:id', async ctx => {
-    const result = await getResumeById(ctx.req.param('id'));
-    return ctx.json(result);
+    const id = ctx.req.param('id');
+    const data = await getResumeById(ctx.req.param('id'));
+    if (!data)
+      throw new HTTPException(404, {
+        message: `Resume with ID '${id}' was not found.`,
+      });
+    return ctx.json(data);
   });
