5986b36bb9b48eec71fc7fd5cdde1dde3d1e395c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-09 17:36:56 +0300

    Add `useResumeById` query hook

---
+++ src/hooks/useResumeById.ts
@@ -0,0 +1,49 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import type { InferResponseType } from 'hono';
+
+import { client } from '@/lib/hono';
+
+export const useResumeById = (id: string, { enabled = true } = {}) => {
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation<
+    InferResponseType<(typeof client.api.resumes)[':id']['$delete']>,
+    Error
+  >({
+    mutationFn: async () => {
+      const res = await client.api.resumes[':id'].$delete({ param: { id } });
+      return await res.json();
+    },
+    // TODO: Toast on success and error
+    onSuccess: () => {
+      // toast.success('Resume deleted.');
+      queryClient.invalidateQueries({ queryKey: ['resumes'] });
+      queryClient.invalidateQueries({ queryKey: ['resume', { id }] });
+    },
+    // onError: () => toast.error("We couldn't delete this resume."),
+  });
+
+  const getQuery = useQuery({
+    enabled:
+      enabled &&
+      !!id &&
+      // Don't try to fetch after deletion, it will result in 404
+      !deleteMutation.isPending &&
+      !deleteMutation.isSuccess,
+    queryKey: ['resume', { id }],
+    queryFn: async () => {
+      const res = await client.api.resumes[':id'].$get({ param: { id } });
+      if (!res.ok) throw new Error('Failed to fetch resume.');
+
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  return {
+    resume: getQuery.data,
+    resumeFetching: getQuery.isFetching,
+    deleteResume: deleteMutation.mutate,
+    deleteResumePending: deleteMutation.isPending,
+  };
+};
