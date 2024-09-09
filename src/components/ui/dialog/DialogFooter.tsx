e3f265b8fb53a18cfaf886cd517b1912727f8547
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-09 19:22:56 +0300

    Update dialog design on mobile

--- src/components/ui/dialog/DialogFooter.tsx
+++ src/components/ui/dialog/DialogFooter.tsx
@@ -7,10 +7,7 @@ export const DialogFooter = ({
   ...props
 }: ComponentPropsWithoutRef<'div'>) => (
   <div
-    className={cn(
-      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
-      className
-    )}
+    className={cn('flex flex-row justify-end space-x-2', className)}
     {...props}
   />
 );
