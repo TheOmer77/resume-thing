8243f70aa7b45ee50dea7d35b48d8fd41b3f99b9
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-09 17:13:27 +0300

    Add `deleteResumeById` query function

--- src/db/queries/resume.ts
+++ src/db/queries/resume.ts
@@ -8,3 +8,11 @@ export const getResumes = async () =>
 
 export const getResumeById = async (id: string): Promise<Resume | undefined> =>
   (await db.select().from(resume).where(eq(resume.id, id)))[0];
+
+export const deleteResumeById = async (id: string) =>
+  (
+    await db
+      .delete(resume)
+      .where(eq(resume.id, id))
+      .returning({ id: resume.id })
+  )[0];
