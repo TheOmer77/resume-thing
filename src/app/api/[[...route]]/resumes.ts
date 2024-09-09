f0a932273475523e744636fe2e77bac7bdbcc317
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-09 17:13:50 +0300

    Add API route to delete resume by ID

--- src/app/api/[[...route]]/resumes.ts
+++ src/app/api/[[...route]]/resumes.ts
@@ -1,7 +1,11 @@
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
 
-import { getResumeById, getResumes } from '@/db/queries/resume';
+import {
+  deleteResumeById,
+  getResumeById,
+  getResumes,
+} from '@/db/queries/resume';
 
 export const resumesRouter = new Hono()
   .get('/', async ctx => {
@@ -11,6 +15,15 @@ export const resumesRouter = new Hono()
   .get('/:id', async ctx => {
     const id = ctx.req.param('id');
     const data = await getResumeById(ctx.req.param('id'));
+    if (!data)
+      throw new HTTPException(404, {
+        message: `Resume with ID '${id}' was not found.`,
+      });
+    return ctx.json(data);
+  })
+  .delete('/:id', async ctx => {
+    const id = ctx.req.param('id');
+    const data = await deleteResumeById(ctx.req.param('id'));
     if (!data)
       throw new HTTPException(404, {
         message: `Resume with ID '${id}' was not found.`,
