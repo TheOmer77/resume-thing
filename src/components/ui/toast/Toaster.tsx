584689082de28e2e45e2966d7f4fb1b47d800992
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-12 15:04:00 +0300

    Update toast styles, icons and theme
    - Use system theme for toasts (default is always light)
    - Add `unstyled: true` and some more classnames
    - Use Lucide icons instead of the Sonner built in ones

--- src/components/ui/toast/Toaster.tsx
+++ src/components/ui/toast/Toaster.tsx
@@ -1,23 +1,41 @@
 'use client';
 
+import {
+  AlertCircleIcon,
+  AlertTriangleIcon,
+  CheckIcon,
+  InfoIcon,
+} from 'lucide-react';
 import type { ComponentPropsWithoutRef } from 'react';
 import { Toaster as Sonner } from 'sonner';
+
+import { Spinner } from '@/components/ui/spinner';
 
 type ToasterProps = ComponentPropsWithoutRef<typeof Sonner>;
 
 export const Toaster = ({ ...props }: ToasterProps) => (
   <Sonner
     className='toaster group'
+    theme='system'
     toastOptions={{
+      unstyled: true,
       classNames: {
         toast:
-          'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+          'toast group group-[.toaster]:flex group-[.toaster]:w-full group-[.toaster]:flex-row group-[.toaster]:items-center group-[.toaster]:gap-2 group-[.toaster]:rounded-lg group-[.toaster]:border group-[.toaster]:border-border group-[.toaster]:bg-background group-[.toaster]:p-4 group-[.toaster]:font-sans group-[.toaster]:text-sm group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
         description: 'group-[.toast]:text-muted-foreground',
         actionButton:
           'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
         cancelButton:
           'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+        icon: 'size-5 [&_svg]:size-5',
       },
+    }}
+    icons={{
+      success: <CheckIcon />,
+      info: <InfoIcon />,
+      warning: <AlertTriangleIcon />,
+      error: <AlertCircleIcon />,
+      loading: <Spinner />,
     }}
     {...props}
   />
