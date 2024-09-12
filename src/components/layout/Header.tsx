69f0e8ab3186853799cdf172c698d6a8feb4bcb7
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-12 19:40:11 +0300

    Tweak header & main mobile padding

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ type TitleProps = PropsWithChildren<{ title?: string }>;
 
 export const Header = ({ title, children }: TitleProps) => (
   <header className='fixed top-0 z-50 w-full bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60 print:hidden'>
-    <div className='container flex h-16 max-w-screen-2xl items-center'>
+    <div className='mx-auto flex h-16 w-full max-w-screen-2xl items-center p-4 sm:px-8'>
       <h1
         className={cn(
           'me-4 flex items-center text-xl font-semibold text-primary lg:me-6',
