b74d2cd0387412cd7696f9d16f5f07ac1ae06a8b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-29 17:34:48 +0300

    Add `outputFileTracingRoot` in next config

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -1,7 +1,12 @@
+import { join } from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: { serverComponentsExternalPackages: ['puppeteer-core'] },
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core'],
+    outputFileTracingRoot: join(import.meta.dirname, '../../'),
+  },
   webpack: config => {
     config.resolve.alias.canvas = false;
     return config;
