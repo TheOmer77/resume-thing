751127a3396e6df1b703135ac4040bcc75f7c53e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-30 18:31:51 +0300

    Add `@resume-thing/eslint-config` package

---
+++ packages/eslint-config/next.js
@@ -0,0 +1,16 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'next/core-web-vitals',
+    './base.js'
+  ],
+  globals: { React: true, JSX: true },
+  env: { node: true },
+  settings: { 'import/resolver': { typescript: { project } } },
+  ignorePatterns: ['.*.js', 'node_modules/'],
+};
