d68f2c3b5d43e6e1910ca54692b6649cb32dbe44
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-02 17:11:17 +0300

    Update eslint next `folder-naming-convention` for app router
    Allow app router naming patterns in Next.js projects app directory (not
    in other directories).

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -7,10 +7,19 @@ module.exports = {
   extends: [
     'plugin:@tanstack/eslint-plugin-query/recommended',
     'next/core-web-vitals',
-    './base.js'
+    './base.js',
   ],
   globals: { React: true, JSX: true },
   env: { node: true },
   settings: { 'import/resolver': { typescript: { project } } },
   ignorePatterns: ['.*.js', 'node_modules/'],
+  rules: {
+    'check-file/folder-naming-convention': [
+      'warn',
+      {
+        'src/app/**': 'NEXT_JS_APP_ROUTER_CASE',
+        'src/!(app)/**/*': 'KEBAB_CASE',
+      },
+    ],
+  },
 };
